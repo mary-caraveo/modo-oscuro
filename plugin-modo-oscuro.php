3814bff26aa308961edbd8b1108baeb700860714
Guadalupe Caraveo <25497052+mary-caraveo@users.noreply.github.com>
2021-11-16 14:02:07 -0500

    fix: formato de código

--- plugin-modo-oscuro.php
+++ plugin-modo-oscuro.php
@@ -7,8 +7,8 @@
 //Author Uri: github
 
 function estilos_plugin(){
-    $estilos_url = plugin_dir_url(__FILE__);
-    wp_enqueue_style( 'estilos_plugin', $estilos_url."/assets/estilos.css", '', '1.0','all');
+  $estilos_url = plugin_dir_url(__FILE__);
+  wp_enqueue_style( 'estilos_plugin', $estilos_url."/assets/estilos.css", '', '1.0','all');
 }
 
 add_action('wp_enqueue_scripts','estilos_plugin');
